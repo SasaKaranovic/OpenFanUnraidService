7ceea03ce9d43bc51f67941bc5e50639476b1f97
Sasa Karanovic <sasa@karanovic.ca>
2025-06-01 00:11:45 -0400

    - Adding support for multiple temperature sensors. Temperature source `TempSource` can now be a list of sensors. - Adding option to specify how `TempSource` list should be evaluated. Default is `max` which finds temperature sensor with the highest temperature and uses it's value for for applying the fan curve. It is possible to explicitly request `min` as the eval function. Although using `min` is probably a really bad idea, but it's there if you need it. - If not specified, default fan curve will be `linear` - If no temperature source (`TempSource`) is specified in fan profile, and error will be raised instead of current silent behaviour. - If temperature sensor does not exist, temperature of `-1` will be returned (previously it would return `0`). Now it is easy to differentiate between missing sensor/disk (`-1`) and a spun-down disk (`0`). If you use LN2 for cooling and this causes issues for you, let me know.

--- src/temperature_sensors.py
+++ src/temperature_sensors.py
@@ -27,12 +27,25 @@ class TemperatureSensors:
     def list_sensors(self):
         return self.sensors
 
-    def get_sensor(self, sensorID):
+    def get_single_sensor(self, sensorID):
         temp = self.sensors.get(sensorID, None)
         if temp is None:
             logger.error(f"Could not find sensor!. (`{sensorID}`)")
-            return 0
+            return -1
 
         if temp == '*':
             return 0
         return int(temp)
+
+    def get_sensors(self, sensor_list):
+        if isinstance(sensor_list, list):
+            temperature_list = []
+            for sensor in sensor_list:
+                temp = self.get_single_sensor(sensor)
+                temperature_list.append(temp)
+            return temperature_list
+
+        if isinstance(sensor_list, str):
+            return [ self.get_single_sensor(sensor_list) ]
+
+        raise TypeError(f"Unsupported sensor type! Function expects either `list` or `str` but `{type(sensor_list)}` was given.")
